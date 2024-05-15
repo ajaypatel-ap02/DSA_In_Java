8fd6acb7feb8757e829afa676c6237a6eb88e3c5
ajaypatel-ap02 <26patilajay@gmail.com>
2024-05-15 09:27:37 +0530

    the max function returning the index of maximum wealth persons

--- src/DSA/maxWealth.java
+++ src/DSA/maxWealth.java
@@ -1,20 +1,33 @@
 package DSA;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class maxWealth {
     public static void main(String[] args) {
-        int[][] accounts = { {4,2,7,2},{1,4,2,3},{8,5,1,4,3,2,6}};
-        System.out.println("The person having maximum wealth is person "+max(accounts));
+        int[][] accounts = { {1,2,3},{3,2,1},{2,1},{5,1}};
+        System.out.println("The person having maximum wealth is person/persons are ");
+        List<Integer> maxWealthPerson = max(accounts);
+        for (Integer wealthPerson : maxWealthPerson) {
+            System.out.print((wealthPerson + 1) + ", ");
+        }
     }
-    static int max(int[][] accounts){
-        int maxWealthPerson = 0;
-        int[] sumOfWealth = new int[accounts.length];
+    static ArrayList<Integer> max(int[][] accounts){
+        ArrayList<Integer> maxWealthPerson = new ArrayList<>();
+        int maxWealth = Integer.MIN_VALUE;
         for (int person = 0; person < accounts.length; person++) {
+            int sumOfWealth = 0;
             for (int account = 0; account < accounts[person].length; account++) {
-                sumOfWealth[person] += accounts[person][account];
+                sumOfWealth += accounts[person][account];
             }
-            if (sumOfWealth[person] > maxWealthPerson) {
-                maxWealthPerson = person;
+            if ( maxWealth == sumOfWealth) {
+                maxWealthPerson.add(person);
             }
+            if (sumOfWealth > maxWealth) {
+                maxWealth = sumOfWealth;
+                maxWealthPerson.add(person);
+            }
+
         }
         return maxWealthPerson;
     }
