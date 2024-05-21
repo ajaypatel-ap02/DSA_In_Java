9933cff96f4d814ff0000d31dcc520989a3c9d85
ajaypatel-ap02 <26patilajay@gmail.com>
2024-05-21 19:21:08 +0530

    Binary search implementation

--- src/DSA/BinarySearch.java
+++ src/DSA/BinarySearch.java
@@ -19,8 +19,7 @@ public class BinarySearch {
     static int search(int[] arr ,int target){
         int start = 0;
         int end = arr.length - 1;
-        if(target > arr[end])
-            return -1;
+
         while (start <= end){
             int mid = start + (end - start) / 2;
             if(arr[mid] == target){
