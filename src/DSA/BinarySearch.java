4dd8d7eaf4421e1bb787aebe94a89b828113fd1e
ajaypatel-ap02 <26patilajay@gmail.com>
2024-05-21 19:17:08 +0530

    finding the ceiling of the target element in the array

--- src/DSA/BinarySearch.java
+++ src/DSA/BinarySearch.java
@@ -19,6 +19,8 @@ public class BinarySearch {
     static int search(int[] arr ,int target){
         int start = 0;
         int end = arr.length - 1;
+        if(target > arr[end])
+            return -1;
         while (start <= end){
             int mid = start + (end - start) / 2;
             if(arr[mid] == target){
