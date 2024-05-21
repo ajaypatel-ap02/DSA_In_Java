15967f08d50c7bb91142606a694ca2f9ea12e627
ajaypatel-ap02 <26patilajay@gmail.com>
2024-05-21 19:22:19 +0530

    binary search implemented for finding the ceiling of the number

--- src/DSA/CeilingOfNumber.java
+++ src/DSA/CeilingOfNumber.java
@@ -15,6 +15,9 @@ public class CeilingOfNumber {
         int start = 0;
         int end = arr.length - 1;
 
+        if(target > arr[end])
+            return -1;
+
         while(start<=end){
             int mid = start + (end - start) / 2;
             if(target == arr[mid] )
