1ee9956dad32d7b207a0a0c88509174e00df45f8
ajaypatel-ap02 <26patilajay@gmail.com>
2024-05-25 23:44:41 +0530

    binary search implemented for find first and last index of the target element

---
+++ src/DSA/FindFirstAndLast.java
@@ -0,0 +1,46 @@
+package DSA;
+
+import java.util.Arrays;
+
+public class FindFirstAndLast {
+
+    public static void main(String[] args) {
+        int[] arr = {5,7,7,7,8,8,10,15,18};
+        int target = 7;
+
+        System.out.println("Answer : "+Arrays.toString(searchFirstAndLast(arr,target))); // Answer : [1, 3];
+    }
+
+    static int[] searchFirstAndLast(int[] arr, int target){
+        int start = binarySearch( arr, target, true) ;
+        int end = binarySearch( arr, target,false);
+        if(start == end){
+            end = -1;
+        }
+        return new int[]{start, end};
+    }
+    static int binarySearch(int[] arr, int target, boolean findStart){
+        int ans = -1;
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+            if(arr[mid] < target){
+                start = mid + 1;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            }else{
+                ans = mid;
+                if(findStart){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
+
+            }
+        }
+        return ans;
+    }
+
+}
