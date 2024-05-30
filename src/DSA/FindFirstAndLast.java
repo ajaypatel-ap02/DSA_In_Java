0857416b0c00cbc49908500f0b58f391d3cf3881
ajaypatel-ap02 <26patilajay@gmail.com>
2024-05-31 01:26:54 +0530

    finds the first and last index of distinct elements

--- src/DSA/FindFirstAndLast.java
+++ src/DSA/FindFirstAndLast.java
@@ -5,19 +5,21 @@ import java.util.Arrays;
 public class FindFirstAndLast {
 
     public static void main(String[] args) {
-        int[] arr = {5,7,7,7,8,8,10,15,18};
+        int[] arr = {7,7,7,8,8,10,15,18};
         int target = 7;
 
         System.out.println("Answer : "+Arrays.toString(searchFirstAndLast(arr,target))); // Answer : [1, 3];
     }
 
     static int[] searchFirstAndLast(int[] arr, int target){
-        int start = binarySearch( arr, target, true) ;
-        int end = binarySearch( arr, target,false);
-        if(start == end){
-            end = -1;
+        int[] ans = {-1, -1};
+        ans[0] = binarySearch( arr, target, true) ;
+         ans[1] = binarySearch( arr, target,false);
+
+        if(ans[0] == ans[1]){
+            ans[1] = -1;
         }
-        return new int[]{start, end};
+        return ans;
     }
     static int binarySearch(int[] arr, int target, boolean findStart){
         int ans = -1;
